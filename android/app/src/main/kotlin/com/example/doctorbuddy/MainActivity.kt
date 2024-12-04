60c53f39eebb6eb0b5ca3d32f347cc81e3bc937f
Soham1250 <154613808+Soham1250@users.noreply.github.com>
2024-12-04 22:28:12 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/doctorbuddy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.doctorbuddy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Doctor Buddy',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Doctor Buddy'),
+      ),
+      body: const Center(
+        child: Text('Welcome to Doctor Buddy'),
+      ),
+    );
+  }
+}
